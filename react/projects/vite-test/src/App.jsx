09e41ec8559b7c65f5aa4abd66cf09054eb9c144
ViktorChs <Victoraltas123@gmail.com>
2023-06-19 11:40:52 -0400

    0.2

--- react/projects/vite-test/src/App.jsx
+++ react/projects/vite-test/src/App.jsx
@@ -1,16 +1,41 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-
-
+const users = [
+    {
+    userName: 'midudev',
+    name: 'Miguel Angel duran',
+    isFollowing: false
+},
+{
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+},
+{
+    userName: 'lofiln',
+    name: 'lofiln',
+    isFollowing: false
+}
+]
 export function App () {
+
     return (
-        <>
-        <TwitterFollowCard userName="pheralb" name="Pablo Hernandez"/>
-        <TwitterFollowCard userName="elonmusk" name="Elon Musk"/>
-        <TwitterFollowCard userName="lofiln" name="slightly private"/>
-
-
-        </>
+        <section className='app'>
+        <h1>Te gustaria seguir a</h1>
+        {
+        users.map(user => {
+            const { userName, name, isFollowing } = user
+            return (
+                <TwitterFollowCard
+                userName={userName}
+                isFollowing={isFollowing}
+                >
+                    {name}
+                </TwitterFollowCard>
+            )
+        })
+        }
+        </section>
     )
 }
--- react/projects/vite-test/src/TwitterFollowCard.jsx
+++ react/projects/vite-test/src/TwitterFollowCard.jsx
@@ -1,4 +1,16 @@
-export function TwitterFollowCard ({userName , name , isFollow}){
+import { useState } from 'react'
+
+export function TwitterFollowCard ({userName , name , isFollow, children}){
+const [isFollowing, setIsFollowing] = useState(false)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+    ? 'tm-followCard-button isfollowing'
+    : 'tm-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
     return (
         <article className='tm-followCard'>
             <header className='tm-followCard-header'>
@@ -7,14 +19,15 @@ export function TwitterFollowCard ({userName , name , isFollow}){
                 alt="El avatar de midudev"
                 src={`https://unavatar.io/${userName}`}/>
                 <div className='tm-followCard-info'>
-                <strong>{name}</strong>
+                <strong>{children}</strong>
                 <span className='tm-followcar-infoUserName'>{userName}</span>
                 </div>
             </header>
 
             <aside>
-                <button className='tm-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tm-followCard-text'>{text}</span>
+                    <span className='tm-followCard-stopFollow '>Dejar de Seguir</span>
                 </button>
             </aside>
         </article>
